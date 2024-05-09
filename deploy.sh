bbf7682326c867440bf82f8d54022596604c89a7
Khanh Khuu <khuu_giang_khanh@colpal.com>
2024-05-09 15:12:21 +0700

    Update CI/CD workflow

--- deploy.sh
+++ deploy.sh
@@ -7,3 +7,15 @@ do
     cp $f ./dist
 done
 clasp push -f
+
+deployments=$(clasp deployments)
+deployment_string=$(echo "$deployments" | sed '3!d')
+id_start=$(echo "$deployment_string" | awk '{print index($0, "-") + 1}')
+deployment_id=$(echo "$deployment_string" | cut -d' ' -f "$id_start")
+deployment_id="${deployment_id##-}"
+
+if [ -z "$deployment_id" ]; then
+    clasp deploy
+else
+    clasp deploy -i "$deployment_id"
+fi
